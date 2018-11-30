1f4708b6025e25baaecb6d66a4b0d239a9aca0d3
MAKHGAL Ganbold <galaagl@yahoo.com>
2018-11-30 11:22:50 +0800

    version 3.3.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [JExtBOX Article Status](http://jextbox.com/jextbox-article-status.html)
 ========
 
-This plugin displays status signs (Just added, Modified, Featured, Popular, Most hit and Archived) before an article. Custom HTML code is allowed in the status message and you can choose or replace default status icons. Copyright (C) 2012-2018 Galaa (http://galaa.mn/). All rights reserved.
+This plugin displays status signs (Just added, Modified, Featured, Popular, Most hit and Archived) before an article. Custom HTML code is allowed in the status message and you can choose or replace default status icons.
 
 Copyright (C) 2012-2018 [Galaa](http://galaa.mn)
--- jextboxarticlestatus.php
+++ jextboxarticlestatus.php
@@ -21,106 +21,117 @@ class plgContentjextboxarticlestatus extends JPlugin
 	public function onContentBeforeDisplay($context, &$article, &$params)
 	{
 
-		$app = JFactory::getApplication();
-		if($app->isAdmin()) return;
-
 		// check excluded views
-		$categories = $this->params->get('categories',array());
-		if((in_array($article->catid, $categories) && ($this->params->get('categories_option') == 'exclude')) || (!in_array($article->catid, $categories) && ($this->params->get('categories_option') == 'include'))){
-			return '';
+
+		if (JFactory::getApplication()->isAdmin() || JFactory::getApplication()->input->get('option', '', 'cmd') != 'com_content') return;
+
+		$categories = $this->params->get('categories', array());
+		$categories_option = $this->params->get('categories_option', 'exclude');
+		$catid_in = in_array($article->catid, $categories);
+		if (($catid_in && $categories_option == 'exclude') || (!$catid_in && $categories_option == 'include')) {
+			return;
 		}
-		$articles = trim($this->params->get('articles'));
-		if(!empty($articles)){
+
+		$articles = trim($this->params->get('articles', ''));
+		if (!empty($articles)) {
 			$articles = explode(',', $articles);
 		}
 		settype($articles, 'array');
-		if((in_array($article->id, $articles) && ($this->params->get('articles_option') == 'exclude')) || (!in_array($article->id, $articles) && ($this->params->get('articles_option') == 'include'))){
-			return '';
+		$articleid_in = in_array($article->id, $articles);
+		$articles_option = $this->params->get('articles_option', 'exclude');
+		if (($articleid_in && ($articles_option == 'exclude')) || (!$articleid_in && ($articles_option == 'include'))) {
+			return;
 		}
 		$view = JFactory::getApplication()->input;
-		if(($view == 'article') && ($this->params->get('show_in_full_view') == 'no')){ 
-			return '';
+		if (($view == 'article') && ($this->params->get('show_in_full_view', 'yes') == 'no')) { 
+			return;
 		}
-		if((($view == 'featured') || ($view == 'frontpage')) && ($this->params->get('show_in_featured_view') == 'no')){ 
-			return '';
+		if ((($view == 'featured') || ($view == 'frontpage')) && ($this->params->get('show_in_featured_view', 'yes') == 'no')) { 
+			return;
 		}
-		if(($view == 'category') && ($this->params->get('show_in_category_view') == 'no')){ 
-			return '';
+		if (($view == 'category') && ($this->params->get('show_in_category_view', 'yes') == 'no')) { 
+			return;
 		}
 
 		// New or Modified
-		$identify_new = $this->params->get('identify_new');
-		$identify_modified = $this->params->get('identify_modified');
+
+		$identify_new = $this->params->get('identify_new', 'yes');
+		$identify_modified = $this->params->get('identify_modified', 'yes');
 
 		// prepare article status sign
-		if($identify_new == 'yes'){
-			if($this->params->get('sign') == 'default'){
+
+		if ($identify_new == 'yes') {
+			if ($this->params->get('sign', 'default') == 'default') {
 				$article_status_new = '<img style="border: 0pt none; vertical-align: middle;" alt="Just Added" title="" src="plugins/content/jextboxarticlestatus/images/just_added.png"/>';
-			}else{
-				$article_status_new = $this->params->get('sign_new_custom');
+			} else {
+				$article_status_new = $this->params->get('sign_new_custom', '');
 			}
 		}
-		if($identify_modified == 'yes'){
-			if ($this->params->get('sign') == 'default'){
+		if ($identify_modified == 'yes') {
+			if ($this->params->get('sign', 'default') == 'default') {
 				$article_status_modified = '<img style="border: 0pt none; vertical-align: middle;" alt="Modified" title="" src="plugins/content/jextboxarticlestatus/images/modified.png"/>';
-			}else{
-				$article_status_modified = $this->params->get('sign_modified_custom');
+			} else {
+				$article_status_modified = $this->params->get('sign_modified_custom', '');
 			}
 		}
 
 		// define article status
+
 		$article_status = '';
 
 		// define New or Modified status
-		if($identify_new == 'yes'){
+
+		if ($identify_new == 'yes') {
 			$created = strtotime($article->created);
 			$articletime = $created;
 			$status = $article_status_new;
 		}
-		if($identify_modified == 'yes'){
+		if ($identify_modified == 'yes') {
 			$modified = strtotime($article->modified);
 			$articletime = $modified;
 			$status = $article_status_modified;
 		}
-		if(($identify_new == 'yes') && ($identify_modified == 'yes')){
-			if (floor(($modified-$created)/86400) >= 1){
+		if (($identify_new == 'yes') && ($identify_modified == 'yes')) {
+			if (floor(($modified - $created) / 86400) >= 1) {
 				$articletime = $modified;
 				$status = $article_status_modified;
-			}else{
+			} else {
 				$articletime = $created;
 				$status = $article_status_new;
 			}
 		}
-		if(floor((time()-$articletime)/86400) <= $this->params->get('days')){
+		if (floor((time() - $articletime) / 86400) <= $this->params->get('days', '7')) {
 			$article_status .= $status;
 		}
 
 		// define Featured status
-		if((($view == 'category') || ($view == 'article')) && ($this->params->get('identify_featured') == 'yes') && ($article->featured == 1)){
-			if($this->params->get('sign') == 'default'){
+
+		if ((($view == 'category') || ($view == 'article')) && ($this->params->get('identify_featured', 'yes') == 'yes') && ($article->featured == 1)) {
+			if ($this->params->get('sign', 'default') == 'default') {
 				$status = '<img style="border: 0pt none; vertical-align: middle;" alt="Featured" title="" src="plugins/content/jextboxarticlestatus/images/featured.png"/>';
-			}else{
-				$status = $this->params->get('sign_featured_custom');
+			} else {
+				$status = $this->params->get('sign_featured_custom', '');
 			}
 			$article_status .= $status;
 		}
 
 		// Hit or Popular status
+
 		$hit_status = '';
 		$db = JFactory::getDBO();
 		$query = 'SELECT `hits` FROM `#__content` WHERE `state`=1 ';
-		if(!empty($categories)){
+		if (!empty($categories)) {
 			$categories = implode(',', $categories);
 			$query .= 'AND `catid` ';
-			if($this->params->get('categories_option') == 'exclude'){
+			if ($this->params->get('categories_option', 'exclude') == 'exclude') {
 				$query .= 'NOT ';
 			}
 			$query .= 'IN ('.$categories.') ';
 		}
 		$articles = trim($this->params->get('articles'));
-		if(!empty($articles)){
+		if (!empty($articles)) {
 			$query .= 'AND `id` ';
-			if($this->params->get('articles_option') == 'exclude'){
+			if ($this->params->get('articles_option', 'exclude') == 'exclude') {
 				$query .= 'NOT ';
 			}
 			$query .= 'IN ('.$articles.') ';
@@ -128,63 +139,62 @@ class plgContentjextboxarticlestatus extends JPlugin
 		$query .= 'ORDER BY `hits` ASC';
 		$db->setQuery($query);
 		$hits_array = $db->loadColumn();
-		$sign_hit_limit = $this->params->get('sign_hit_limit',0.9);
+		$sign_hit_limit = $this->params->get('sign_hit_limit', 0.9);
 		$sign_hit_limit = $hits_array[floor($sign_hit_limit * count($hits_array))];
-		$sign_popular_limit = $this->params->get('sign_popular_limit',0.75);
+		$sign_popular_limit = $this->params->get('sign_popular_limit', 0.75);
 		$sign_popular_limit = $hits_array[floor($sign_popular_limit * count($hits_array))];
 		unset($hits_array);
 
 		// define Most Hit status
-		if(($this->params->get('identify_hit') == 'yes') && ($article->hits >= $sign_hit_limit)){
-			if($this->params->get('sign') == 'default'){
+
+		if (($this->params->get('identify_hit', 'yes') == 'yes') && ($article->hits >= $sign_hit_limit)) {
+			if ($this->params->get('sign', 'default') == 'default') {
 				$status = '<img style="border: 0pt none; vertical-align: middle;" alt="Most Hit" title="" src="plugins/content/jextboxarticlestatus/images/most_hit.png"/>';
-			}else{
-				$status = $this->params->get('sign_hit_custom');
+			} else {
+				$status = $this->params->get('sign_hit_custom', '');
 			}
 			$hit_status = $status;
 		}
 
 		// define Popular status
-		if(($this->params->get('identify_popular') == 'yes') && ($hit_status == '') && ($article->hits >= $sign_popular_limit)){
-			if($this->params->get('sign') == 'default'){
+
+		if (($this->params->get('identify_popular', 'yes') == 'yes') && ($hit_status == '') && ($article->hits >= $sign_popular_limit)) {
+			if ($this->params->get('sign', 'default') == 'default') {
 				$status = '<img style="border: 0pt none; vertical-align: middle;" alt="Popular" title="" src="plugins/content/jextboxarticlestatus/images/popular.png"/>';
-			}else{
-				$status = $this->params->get('sign_popular_custom');
+			} else {
+				$status = $this->params->get('sign_popular_custom', '');
 			}
 			$hit_status = $status;
 		}
 
 		// Hit or Popular status
+
 		$article_status .= $hit_status;
 
 		// define Archived status
+
 		$archived_status = '';
-		if(($this->params->get('identify_archived') == 'yes') && isset($article->state) && ($article->state == 2)){
-			if($this->params->get('sign') == 'default'){
+		if (($this->params->get('identify_archived') == 'yes') && isset($article->state) && ($article->state == 2)) {
+			if($this->params->get('sign', 'default') == 'default') {
 				$status = '<img style="border: 0pt none; vertical-align: middle;" alt="Archived" title="" src="plugins/content/jextboxarticlestatus/images/archived.png"/>';
 			}else{
-				$status = $this->params->get('sign_archived_custom');
+				$status = $this->params->get('sign_archived_custom', '');
 			}
 			$archived_status = $status;
 		}
 
 		// Archived status
+
 		$article_status .= $archived_status;
 
 		// DIV
-		if($this->params->get('within_a_div') == 'yes'){
-			$div = '<div';
-			$div_name = trim($this->params->get('div_name'));
-			$div_name = !empty($div_name) ? $div_name : 'jextboxarticlestatus';
-			if($this->params->get('class_or_id') == 'id'){
-				$div .= ' id="' . $div_name .  '"';
-			}else{
-				$div .= ' class="' . $div_name .  '"';
-			}
-			$style = trim($this->params->get('div_style'));
+
+		if ($this->params->get('wrap_by_div', 'no') == 'yes') {
+			$class = trim($this->params->get('div_css_class_name', ''));
+			$class = !empty($class) ? ' class="' . $class . '"' : '';
+			$style = trim($this->params->get('div_css_style', ''));
 			$style = !empty($style) ? ' style="' . $style . '"' : '';
-			$div .= $style . '>';
-			$article_status = $div . $article_status . '</div>';
+			$article_status = '<div' . $class . $style . '>' . $article_status . '</div>';
 		}
 
 		// article status
